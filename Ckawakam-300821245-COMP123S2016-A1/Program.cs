9ec246c581841bb7fbdcff198057cc10c9a4066f
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-13 16:24:16 -0400

    Added comment

--- Ckawakam-300821245-COMP123S2016-A1/Program.cs
+++ Ckawakam-300821245-COMP123S2016-A1/Program.cs
@@ -3,7 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * Author: Chinatsu Kawakami
+ * Date Created: May 12, 2016
+ * Date Modified: May 13, 2016
+ * Student ID: 300821245
+ * 
+ * Discription: This program demostrates GitHub and version control
+ * best practices. We also explore Unit Testing
+ * 
+ * Version: 0.1 - Added comment
+ */
 namespace Ckawakam_300821245_COMP123S2016_A1
 {
    public class Program
@@ -13,7 +23,7 @@ namespace Ckawakam_300821245_COMP123S2016_A1
             // call the hello world to console method
             HelloWorldToConsole();
         }
-        /*
+        /**
          * @discription: This is my HelloWorldMethod
          * @method: HelloWorldToConsole
          * @return: {string} helloworldString
