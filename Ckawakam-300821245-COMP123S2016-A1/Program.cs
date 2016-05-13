6923fd171dc0d9acdc3a9c896f1ae63be209a50e
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2016-05-13 01:15:22 -0400

    Added gitignore and gitattributes

---
+++ Ckawakam-300821245-COMP123S2016-A1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ckawakam_300821245_COMP123S2016_A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
